e41347621401233cb461d365da6fa313b48e642a
rionagreally <rionagreally@gmail.com>
2017-11-08 13:24:43 +0000

    uploading files working

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,  request, redirect, url_for, send_from_directory
 
 import flask as fl
+import json
+import os
 
+
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = fl.Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+from werkzeug import SharedDataMiddleware
+app.add_url_rule('/uploads/<filename>', 'uploaded_file', build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
 
-@app.route("/")
-def route():
-	return "Hello World"
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+           #filename = secure_filename(file.filename)
+           filename = file.filename
+           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template("index.html")
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 if __name__ == "__main__":
 	app.run()
